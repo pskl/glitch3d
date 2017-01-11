15bf8dc04c67103bcf6e72e338ce0e3f35b68a3c
Pascal Lamouric <pascal.lamouric@gmail.com>
2017-01-11 16:39:47 +0100

    Revamp automated rendering process

--- lib/glitch3d/rendering_script.py
+++ lib/glitch3d/rendering_script.py
@@ -1,13 +1,22 @@
 # Rendering script
 # Run by calling the blender executable with -b -P <script_name>
+#
+# process:
+# 1) Load model given as a parameter
+# 2) Create lamps
+# 3) Create camera with constraint on rotation
+# 4) Move camera around object and render shot at each step
 
 import bpy
 import os
 import argparse
 
-LIGHT_INTENSITY = 2.0
-lamps =[(7.47, -7.22, 0.97), (-7.63, -7.22, 0.97)]
-cams = [[(-16.31, -15.55, 10), (1.26, 0.009, -0.81)], [(17.43, -14.49, 5.0), (1.45, 0.00, 0.87)]]
+LIGHT_INTENSITY = 1.5
+SHOTS_NUMBER = 4
+
+# TODO: generic lamp positions according to mesh size
+LAMP_POSITIONS = [(7.47, -7.22, 0.97), (-7.63, -7.22, 0.97)]
+# cams = [[(-16.31, -15.55, 10), (1.26, 0.009, -0.81)], [(17.43, -14.49, 5.0), (1.45, 0.00, 0.87)]]
 
 def camera_location_string(camera):
     return str(camera.rotation_euler.x) + ' ' + str(camera.rotation_euler.y) + ' ' + str(camera.rotation_euler.z)
@@ -25,47 +34,55 @@ def get_args():
 
   # add parser rules
   parser.add_argument('-f', '--file', help="obj file to render")
+  parser.add_argument('-n', '--shots_number', help="number of shots")
   parsed_script_args, _ = parser.parse_known_args(script_args)
   return parsed_script_args
 
 args = get_args()
 context = bpy.context
-models = [args.file]
-scene = bpy.data.scenes.new("Automated Render Scene")
-camera_objects = []
 
-# Add cameras
-for index, cam in enumerate(cams):
-    camera_data = bpy.data.cameras.new('Camera' + str(index))
-    camera = bpy.data.objects.new('Camera' + str(index), camera_data)
-    camera_objects.append(camera)
-    camera.location = cam[0]
-    camera.rotation_euler = cam[1]
-    scene.objects.link(camera)
+new_scene = bpy.data.scenes.new("Automated Render Scene")
 
+# Load model
+bpy.ops.import_scene.obj(filepath=os.path.join(args.file), use_edges=True)
+
+# -----------------------------------------
+# Create camera with constraint on rotation
+# -----------------------------------------
+
+camera_data = bpy.data.cameras.new('Camera')
+camera_object = bpy.data.objects.new('Camera', camera_data)
+camera_constraint = camera_object.constraints.new(type='TRACK_TO')
+camera_constraint.target =
+new_scene.objects.link(camera)
+context.scene.camera = camera
+
+# ---------
 # Add lamps
+# ---------
+
 for lamp in lamps:
     lamp_data = bpy.data.lamps.new(name="Lamp", type='POINT')
     lamp_data.energy = LIGHT_INTENSITY
     lamp_object = bpy.data.objects.new(name="Lamp", object_data=lamp_data)
     lamp_object.location = lamp
     lamp_object.select = True
-    scene.objects.link(lamp_object)
+    new_scene.objects.link(lamp_object)
 
-context.screen.scene = scene
-bpy.ops.scene.new(type='LINK_OBJECTS')
-context.scene.name = 'Automated Render Scene'
+context.screen.scene = new_scene
 
-for model_path in models:
-    path = os.path.join(model_path)
+# ------
+# Shoot
+# ------
 
-    # Load model
-    bpy.ops.import_scene.obj(filepath=path, use_edges=True)
+for index in range(0, args.shots_number):
+    context.scene.camera.position =
+    print('Camera now at position: ' + camera_location_string(camera) + ' / rotation:' + camera_rotation_string(camera))
+    context.scene.render.filepath = 'renders/' + os.path.splitext(model_path)[0].split('/')[1] + '_' + str(index) + '.png'
 
-    for index, cam in enumerate(camera_objects):
-        # Loop through cameras and take shots
-        print('Camera now at position:' + camera_location_string(cam) + ' / rotation:' + camera_rotation_string(cam))
-        context.scene.camera = cam
-        context.scene.render.filepath = 'renders/' + os.path.splitext(model_path)[0].split('/')[1] + '_' + str(index) + '.png'
-        scene.update()
-        bpy.ops.render.render(write_still=True, use_viewport=True)
+    context.scene.render.resolution_x = 1920
+    context.scene.render.resolution_y = 1080
+
+    bpy.context.scene.update()
+    print('Rendering image with resolution : ' + str(bpy.context.scene.render.resolution_x) + ' x ' + str(bpy.context.scene.render.resolution_y))
+    bpy.ops.render.render(write_still=True, use_viewport=True)
