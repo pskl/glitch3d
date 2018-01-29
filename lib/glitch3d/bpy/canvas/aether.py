ba1004f379144dde21638ca10076fd818f50abed
Pascal Lamouric <hello@pascal.cc>
2018-01-29 10:48:40 +0100

    Amend fluid sim config

--- lib/glitch3d/bpy/canvas/aether.py
+++ lib/glitch3d/bpy/canvas/aether.py
@@ -10,11 +10,13 @@ container = last_added_object('CUBE')
 container.name = 'fluid_container'
 container.modifiers.new(name='container', type='FLUID_SIMULATION')
 container.modifiers['container'].settings.type = 'DOMAIN'
-container.modifiers['container'].settings.generate_particles = 1
+container.modifiers['container'].settings.generate_particles = 5
 container.modifiers['container'].settings.surface_subdivisions = 100
 container.modifiers['container'].settings.viscosity_exponent = 6
 container.modifiers['container'].settings.viscosity_base = 1.0
+container.modifiers['container'].settings.resolution = 100
 container.modifiers['container'].settings.simulation_scale = 1
+container.modifiers['container'].settings.simulation_rate = 5
 
 def spawn_emitter_fuild(location, emission_vector):
   bpy.ops.mesh.primitive_uv_sphere_add(location=location)
@@ -33,8 +35,8 @@ def make_object_fluid_collider(obj):
     obj.modifiers['obstacle'].settings.volume_initialization = 'BOTH'
     obj.modifiers['obstacle'].settings.partial_slip_factor = 0.15
 
-spawn_emitter_fuild((-2,0,8),mathutils.Vector((0, 0, -1)))
-spawn_emitter_fuild((0,2,8),mathutils.Vector((0, 0, -1)))
+spawn_emitter_fuild((-2,0,8),mathutils.Vector((-0.25, -0.25, -2)))
+spawn_emitter_fuild((0,2,8),mathutils.Vector((-0.25, -0.25, -2)))
 
 assign_material(container, fetch_material('colorshift'))
 
