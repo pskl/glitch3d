52dc4277622f570da6c30bd8daa89be33d7b7e2a
Pascal Lamouric <hello@pascal.cc>
2018-02-11 17:11:58 +0100

    Add average post-processing treatment

---
+++ lib/glitch3d/bpy/post-processing/average.py
@@ -0,0 +1,45 @@
+# Use Pillow fork with Python3
+from PIL import Image
+from PIL import ImageChops
+from PIL import ImageEnhance
+import numpy as np
+
+import sys, os, cv2
+
+print("Averaging")
+path = os.environ['RENDER_PATH']
+files = [path + f for f in os.listdir(path) if (os.path.isfile(os.path.join(path, f)) and os.path.join(path, f).endswith(".png"))]
+average_image = ''
+for file in files:
+    image = np.asarray((Image.open(file)))
+    # Extract the frame and convert to image
+    average_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGBA)
+    average_image = Image.fromarray(average_image)
+    old_image = average_image
+
+    # Extract the frame and convert to image
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGBA)
+    image = Image.fromarray(image)
+
+    # Calculate the difference between this frame and the last
+    diff = ImageChops.difference(image, old_image)
+
+    # Store the image for use in the next itteration
+    old_image = image
+
+    # Convert to greyscale and use that as the alpha channel
+    gray_image = diff.convert('L')
+    diff.putalpha(gray_image)
+
+    #average_image = Image.blend(average_image,image,0.1)
+    #average_image = Image.alpha_composite(average_image,diff)
+    average_image = ImageChops.lighter(average_image,diff)
+
+    # Darken the image slightly to prevent it getting washed out
+    average_image = average_image.point(lambda p: p * 0.9)
+
+    # Show a preview of the image
+    # average_image.show()
+    average_image.save(file + "_average.png")
+
+average_image.save(path + "average.png")
